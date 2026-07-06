a1bba1b79bafde54ec00c623fcb33efcb513d5be
ToilaChi <chi12345pham@gmail.com>
2026-07-06 17:02:49 +0700

    feat: add TaskRepository

---
+++ todolist/src/main/java/com/example/todolist/repository/TaskRepository.java
@@ -0,0 +1,8 @@
+package com.example.todolist.repository;
+
+import com.example.todolist.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificationExecutor<Task> {
+}
